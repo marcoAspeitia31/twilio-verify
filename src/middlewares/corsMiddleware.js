e4585e265486e0bc5b00c0b9f49efa8ce3fa66d5
Marco Aspeitia <maspeitia.31@gmail.com>
2025-06-18 22:53:36 -0600

    test in docer vps

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,16 +1,32 @@
 import cors from 'cors';
 
 const allowedOrigins = process.env.CORS_ORIGINS?.split(',') || [];
+const allowedMethods = ['GET', 'POST', 'OPTIONS'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Aceptar si no hay origin (Postman, Apphive) o si está en la lista blanca
-    if (!origin || allowedOrigins.includes(origin) || allowedOrigins.includes('*')) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true // Si deseas permitir cookies/autenticación cruzada
+const corsOptionsDelegate = function (req, callback) {
+  const origin = req.header('Origin');
+
+  if (!origin || allowedOrigins.includes(origin) || allowedOrigins.includes('*')) {
+    callback(null, {
+      origin: true,
+      methods: allowedMethods,
+      credentials: true
+    });
+  } else {
+    callback(new Error('CORS_ORIGIN_NOT_ALLOWED'), null);
+  }
 };
 
-export default cors(corsOptions);
+const corsMiddleware = cors(corsOptionsDelegate);
+
+function handleCorsError(err, req, res, next) {
+  if (err?.message === 'CORS_ORIGIN_NOT_ALLOWED') {
+    return res.status(403).json({
+      error: 'Origin not allowed by CORS policy',
+      allowedOrigins
+    });
+  }
+  next(err);
+}
+
+export { corsMiddleware, handleCorsError };
